71408fb3ea55e81dafde167cdd90697f3f47cd05
mickdassive <adenandreana@gmail.com>
2023-01-09 21:49:42 -0500

    added gpl3 headder

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 // reflow oven firmware
 // v0.1
-// 12/30/22
+// 1/9/2023
 // Tokyo Andreana
+//{reflow oven firmware}
+//Copyright (C) {2023}  {mickmake}
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
